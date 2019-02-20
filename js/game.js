ca47e0f9ebdf34e078fd9fc2878e8b91110cd077
philipid3s <julien.regnault@gmail.com>
2019-02-20 11:56:25 +0800

    tileset

--- js/game.js
+++ js/game.js
@@ -4,30 +4,45 @@
 
 kontra.init();
 
-let sprite = kontra.sprite({
-    x: 100,        // starting x,y position of the sprite
-    y: 80,
-    color: 'blue',  // fill color of the sprite rectangle
-    width: 20,     // width and height of the sprite rectangle
-    height: 20,
-    // dx: 2          // move the sprite 2px to the right every frame
-});
+// set default asset paths
+// kontra.assets.imagePath = 'imgs/';
+
+// load assets
+
+kontra.assets.load(
+  // tilesets
+  '../imgs/roguelikeDungeon_transparent.png', '../data/marginTiles.json', '../data/rogueLikeDungeon_transparent.json'
+).then(function() {
+
+  let tileEngine = kontra.tileEngine(kontra.assets.data['../data/marginTiles']);
+  // tileEngine.render();
+
+  console.log(tileEngine);
+
+  let player = kontra.sprite({
+      x: 50,        // starting x,y position of the sprite
+      y: 50,
+      color: 'blue',  // fill color of the sprite rectangle
+      width: 20,     // width and height of the sprite rectangle
+      height: 20,
+      // dx: 2          // move the sprite 2px to the right every frame
+  });
   
-let limitX = kontra.canvas.width - sprite.width;
-let limitY = kontra.canvas.height - sprite.height;
+  let limitX = kontra.canvas.width - player.width;
+  let limitY = kontra.canvas.height - player.height;
 
-/**
- *  Object pools
- */ 
-let enemies = kontra.pool({
-    create: kontra.sprite,
-    maxSize: 10
-});
+  /**
+   *  Object pools
+   */ 
+  let enemies = kontra.pool({
+      create: kontra.sprite,
+      maxSize: 10
+  });
 
-let enemyBullets = kontra.pool({
-    create: kontra.sprite,
-    maxSize: 30
-});
+  let enemyBullets = kontra.pool({
+      create: kontra.sprite,
+      maxSize: 30
+  });
 
 /**
 * Spawn a new wave of enemies (6 enemies)
@@ -95,38 +110,53 @@ function spawnWave() {
 
 let loop = kontra.gameLoop({  // create the main game loop
     update: function() {        // update the game state
-      sprite.update();
+      player.update();
 
       enemies.update();
       enemyBullets.update();
   
       // wrap the sprites position when it reaches
       // the edge of the screen
-      if (sprite.x > limitX) {
-        sprite.x = limitX;
+      if (player.x > limitX) {
+        player.x = limitX;
       }
-      if (sprite.x < 0) {
-        sprite.x = 0;
+      if (player.x < 0) {
+        player.x = 0;
       }
 
-      if (sprite.y > limitY) {
-        sprite.y = limitY;
+      if (player.y > limitY) {
+        player.y = limitY;
       }
-      if (sprite.y < 0) {
-        sprite.y = 0;
+      if (player.y < 0) {
+        player.y = 0;
       }
 
       if (kontra.keys.pressed('right')) {
-          sprite.x += 2;
+        player.x += 2;
       }
       if (kontra.keys.pressed('left')) {
-        sprite.x -= 2;
+        player.x -= 2;
       }
       if (kontra.keys.pressed('up')) {
-        sprite.y -= 2;
+        player.y -= 2;
       }
       if (kontra.keys.pressed('down')) {
-        sprite.y += 2;
+        player.y += 2;
+      }
+      
+      if(tileEngine.layerCollidesWith('collision', player)) {
+        if (kontra.keys.pressed('right')) {
+          player.x -= 2;
+        }
+        if (kontra.keys.pressed('left')) {
+          player.x += 2;
+        }
+        if (kontra.keys.pressed('up')) {
+          player.y += 2;
+        }
+        if (kontra.keys.pressed('down')) {
+          player.y -= 2;
+        }
       }
 
       // spawn a new wave of enemies
@@ -135,7 +165,10 @@ let loop = kontra.gameLoop({  // create the main game loop
       }
     },
     render: function() {        // render the game state
-      sprite.render();
+      // background
+      tileEngine.render();
+
+      player.render();
 
       enemies.render();
       enemyBullets.render();
@@ -143,3 +176,4 @@ let loop = kontra.gameLoop({  // create the main game loop
 });
   
 loop.start();    // start the game
+});
